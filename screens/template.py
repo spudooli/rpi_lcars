89e1c5ba5d29e2853cf6ee0d04791a228462d0d0
MS3FGX <MS3FGX@gmail.com>
2016-10-23 13:13:40 -0400

    Add template file for additional screens

---
+++ screens/template.py
@@ -0,0 +1,69 @@
+from subprocess import call
+from datetime import datetime
+import pygame
+from pygame.mixer import Sound
+
+from ui import colours
+from ui.widgets.background import LcarsBackgroundImage, LcarsImage
+from ui.widgets.gifimage import LcarsGifImage
+from ui.widgets.lcars_widgets import LcarsText, LcarsButton
+from ui.widgets.screen import LcarsScreen
+from ui.widgets.sprite import LcarsMoveToMouse
+
+# Need to change class name to whatever screen is to be called
+class ScreenTemplate(LcarsScreen):
+    def setup(self, all_sprites):
+        # Load BG image
+        all_sprites.add(LcarsBackgroundImage("assets/lcars_bg.png"), layer=0)
+
+        # Time/Date display
+        self.stardate = LcarsText(colours.BLUE, (12, 380), "", 1.5)
+        self.lastClockUpdate = 0
+        all_sprites.add(self.stardate, layer=1)
+
+        # Static text
+        all_sprites.add(LcarsText(colours.BLACK, (8, 40), "LCARS 1123"), layer=1)
+        all_sprites.add(LcarsText(colours.ORANGE, (0, 135), "SECTION NAME", 2), layer=1)
+
+        # Interfaces
+        all_sprites.add(LcarsButton(colours.RED_BROWN, "btn", (6, 662), "MAIN", self.logoutHandler), layer=2)
+        all_sprites.add(LcarsButton(colours.BEIGE, "nav", (145, 15), "BUTTON 1", self.nullfunction), layer=2)
+        all_sprites.add(LcarsButton(colours.PURPLE, "nav", (200, 15), "BUTTON 2", self.nullfunction), layer=2)
+        all_sprites.add(LcarsButton(colours.BLUE, "nav", (255, 15), "BUTTON 3", self.nullfunction), layer=2)
+        all_sprites.add(LcarsButton(colours.ORANGE, "nav", (310, 15), "BUTTON 4", self.nullfunction), layer=2)
+        all_sprites.add(LcarsButton(colours.PEACH, "nav", (365, 15), "", self.nullfunction), layer=2)
+
+        # Info text
+        all_sprites.add(LcarsText(colours.BLUE, (244, 174), "TEXT GOES HERE", 1.5), layer=3)
+        self.info_text = all_sprites.get_sprites_from_layer(3)
+
+        # SFX
+        self.beep1 = Sound("assets/audio/panel/201.wav")
+        Sound("assets/audio/hail_2.wav").play()
+
+    def update(self, screenSurface, fpsClock):
+        if pygame.time.get_ticks() - self.lastClockUpdate > 1000:
+            self.stardate.setText("{}".format(datetime.now().strftime("%a %b %d, %Y - %X")))
+            self.lastClockUpdate = pygame.time.get_ticks()
+        LcarsScreen.update(self, screenSurface, fpsClock)
+
+    def handleEvents(self, event, fpsClock):
+        LcarsScreen.handleEvents(self, event, fpsClock)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.beep1.play()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            return False
+
+    def hideInfoText(self):
+        if self.info_text[0].visible:
+            for sprite in self.info_text:
+                sprite.visible = False
+
+    def nullfunction(self, item, event, clock):
+        print("I am a fish.")
+
+    def logoutHandler(self, item, event, clock):
+        from screens.main import ScreenMain
+        self.loadScreen(ScreenMain())
