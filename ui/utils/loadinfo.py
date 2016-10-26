3d2ca6d63a2c9b31b321c952725192e15667b4ff
MS3FGX <MS3FGX@gmail.com>
2016-10-25 20:46:54 -0400

    Add new system functions to loadinfo.py

--- ui/utils/loadinfo.py
+++ ui/utils/loadinfo.py
@@ -1,15 +1,16 @@
-# Read and process data from files
+# Read and process data from files/system
+import subprocess
 
 # Return lines of text
-def readtxt(filename):
+def read_txt(filename):
     lines = []
     with open(filename, mode='r') as infile:
-        lines = infile.read().splitlines() 
+        lines = infile.read().splitlines()
 
     return lines;
 
 # Return CSV pairs
-def readcsv(filename):
+def read_csv(filename):
     lines = []
     with open(filename, mode='r') as infile:
         for line in infile.readlines():
@@ -25,3 +26,31 @@ def get_ip_status(sourcelist, index):
     status = str(sourcelist[index])[18:-2]
     ipstatus = {"ip": ipaddr, "status": status}
     return ipstatus;
+
+# Return current uptime
+def get_uptime():
+    ps = subprocess.Popen("uptime -p", shell=True, stdout=subprocess.PIPE)
+    uptime = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+    return uptime.rstrip()[3:];
+
+# Return system load
+def get_load():
+    ps = subprocess.Popen("uptime", shell=True, stdout=subprocess.PIPE)
+    uptime = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+    return uptime.rstrip()[54:];
+
+# Check version against GitHub
+def update_available():
+    ps = subprocess.Popen("git remote update >/dev/null 2>&1 && git status | grep behind | wc -l", shell=True, stdout=subprocess.PIPE)
+    gitstatus = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+    # Return value
+    if gitstatus[0] == "0":
+        return 0;
+    elif gitstatus[0] == "1":
+        return 1;
