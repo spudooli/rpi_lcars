e2bc2194ed6b544708a22f13e46b68d40d8b54fe
Dave <dave@spudooli.com>
2020-01-06 18:51:58 +1300

    Starts the script that gets the data

---
+++ scripts/get-the-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run by cron
+# Every minute get the data
+
+#Power consumption now
+scp dave@192.168.1.2:/var/www/spudooli/power.txt home/pi/rpi_lcars/scripts/power.txt
+
+#Something
+scp dave@192.168.1.2:/var/www/scripts/otherbalance.txt /home/pi/rpi_lcars/scripts/otherbalance.txt
+
+#Weather
+
+
+
