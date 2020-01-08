fda6428c34ac44321cc23dbd868ed3658a8fddfe
Dave <dave@spudooli.com>
2020-01-08 19:22:50 +1300

    less brackets

--- scripts/ssh-commands.sh
+++ scripts/ssh-commands.sh
@@ -2,12 +2,10 @@
 touch /tmp/yep.txt
 if [ $1 == "alllightson" ]
 then
-    touch /tmp/on.txt
     mosquitto_pub -h 192.168.1.2 -p 1883 -t "house/lights/all" -m "on" -q 1
 fi
 
 if [ $1 == "alllightsoff" ]
 then
-    touch /tmp/off.txt
     mosquitto_pub -h 192.168.1.2 -p 1883 -t "house/lights/all" -m "off" -q 1
 fi
