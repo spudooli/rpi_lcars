0d0ff2c241fbc103987f5eb2c640b1f3c6d983ef
Dave <dave@spudooli.com>
2020-01-08 18:56:47 +1300

    Change to mosquitto instead

--- scripts/ssh-commands.sh
+++ scripts/ssh-commands.sh
@@ -3,10 +3,10 @@ touch /tmp/yep.txt
 if [ $1 == "alllightson" ]
 then
     touch /tmp/on.txt
-    ssh dave@192.168.1.2 'bash /var/www/scripts/lights.sh alllightson'
+    mosquitto_pub -h 192.168.1.2 -p 1883 -t "house/lights/all" -m "on" -q 1
 fi
 
 if [ $1 == "alllightsoff" ]
 then
-    ssh dave@192.168.1.2 'bash /var/www/scripts/lights.sh alllightsoff'
+    mosquitto_pub -h 192.168.1.2 -p 1883 -t "house/lights/all" -m "off" -q 1
 fi
