63ab8b88aeab3f82813b01dfb7f031d36ee4bfbd
Dave <dave@spudooli.com>
2020-01-13 19:09:48 +1300

    Too many

--- scripts/button.py
+++ scripts/button.py
@@ -5,10 +5,10 @@ from rpi_backlight import Backlight
 backlight = Backlight()
 
 def backlightchanger():
+    print("pressed")
     if not backlight.power:
         print("Backlight is off, turning it on now")
-        with backlight.fade(duration=1):
-            backlight.brightness = 100
+        backlight.power = True
 
     if backlight.power:
         print("Backlight is on, turning it off")
@@ -17,7 +17,7 @@ def backlightchanger():
             backlight.power = False
 
 
+button1 = Button(21,bounce_time=2)
 
 while True:
-    button1 = Button(21,bounce_time=2)
     button1.when_pressed = backlightchanger
