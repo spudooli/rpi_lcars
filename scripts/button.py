df4e0f2335d9b16a36066798a444a02d411d41eb
Dave <dave@spudooli.com>
2020-01-13 14:32:08 +1300

    Change backlight control to rpi_backlight

--- scripts/button.py
+++ scripts/button.py
@@ -1,19 +1,22 @@
 from gpiozero import Button
 from signal import pause
-import subprocess
+from rpi_backlight import Backlight
 
-def backlight():
-    bl = open(BASEDIR + "/sys/class/backlight/rpi_backlight/bl_power", "r")
-    backlightState = ob.read()
-    if backlightState =="1"
+backlight = Backlight()
+
+def backlightchanger():
+    if backlight.brightness > "0"
         print("Backlight is off, turning it on now")
-        subprocess.Popen(['echo','0','|','sudo','tee','/sys/class/backlight/rpi_backlight/bl_power'
+        with backlight.fade(duration=1):
+            backlight.brightness = 100
+
     if backlightState =="0":
         print("Backlight is on, turning it off")
-        subprocess.Popen(['echo','1','|','sudo','tee','/sys/class/backlight/rpi_backlight/bl_power'
+        with backlight.fade(duration=1):
+            backlight.brightness = 0
 
 button1 = Button(21,bounce_time=2)
 
-button1.when_pressed = backlight
+button1.when_pressed = backlightchanger
 
 pause()
